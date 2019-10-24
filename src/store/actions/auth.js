6b9782f2aa24f7b439db21f6b710979c513f20d5
Sushant <DELL@DESKTOP-EJJ66O4.(none)>
2019-10-24 23:13:46 +0530

    update

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './actionTypes'
-import axios from 'axios'
+import axios from '../../axios-base'
 import jwt_decode from 'jwt-decode';
 export const authStart = () => {
     return {
