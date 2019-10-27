7048e322109fef1f02ebfad56939ad45584823e6
Sushant <DELL@DESKTOP-EJJ66O4.(none)>
2019-10-27 18:36:40 +0530

    update

--- src/axios-base.js
+++ src/axios-base.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 const developmentUrl = 'http://localhost:3002/';
 const productionUrl = 'http://peaceful-waters-64481.herokuapp.com/'
 const instance = axios.create({
-    baseURL: developmentUrl
+    baseURL: productionUrl
 })
 export default instance
